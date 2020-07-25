136348b3de89bc1fb806137c67e970caff5c490e
Michael Walter <michael@MacBook-Pro.local>
2020-07-25 16:59:00 -0700

    Completed Problem Solving Steps 1 and 2.

---
+++ Section_4/20_step_2_concrete_examples.js
@@ -0,0 +1,21 @@
+
+// Step 2: Concrete Examples
+
+/*
+
+    Explore Concrete Examples:
+
+        1. Start with Simple Examples.
+        2. Progress to More Complex Examples.
+        3. Explore Examples with Empty Inputs
+        4. Explore Examples with Invalid Inputs.
+
+*/
+
+// Write a function which takes in a string and returns 
+// the counts of each character in the string.
+
+charCount("aaaa")  // {a:4}
+charCount("hello") // {h:1,e:1,l:2,o:1}
+
+charCount("")   // return error
